c18f2d6e1480f54551cf227b77b0778633c9b285
ilwoo <ooo952@naver.com>
2025-04-11 17:59:37 +0900

    20250411 boj22300

---
+++ src/boj/BOJ23300.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ23300 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+
+        Stack<Integer> stackPrev = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
+    }
+}
